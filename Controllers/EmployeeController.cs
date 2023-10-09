8dc2cca68b55a2262fcbffe94bad987f64e3b75f
ozmen1 <44169946+ozmen1@users.noreply.github.com>
2023-10-09 21:08:32 +0300

    publish

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Basics.Models;
+
+namespace Basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index1(){
+
+            string message = $"Hello World. {DateTime.Now.ToString()}";
+            return View("Index1", message);
+        }
+
+        public ViewResult Index2(){
+            var names = new String[]
+            {
+                "Kerem",
+                "Merem",
+                "lerem"
+            };
+            return View(names);
+        }
+
+        public IActionResult Index3(){
+            var list = new List<Employee>()
+            {
+                new Employee(){Id=1, FirstName="Ahmet", LastName="Can", Age=20},
+                new Employee(){Id=2, FirstName="Can", LastName="Dağ", Age=25},
+                new Employee(){Id=3, FirstName="Demir", LastName="Güneş", Age=37}
+            };
+            return View("Index3", list);
+        }
+    }
+}
